9453702ba4384217f313ffe4a6ecc5376a73c2b7
Max Strandberg <maax.strandberg@gmail.com>
2023-03-11 13:01:46 +0100

    second commit, gameover + reset + oil replace with dirt after mine

--- index.js
+++ index.js
@@ -15,8 +15,12 @@ let playerDown;
 let playerRight;
 let playerNorm;
 
-let gameOverScreen;
+let gameRunning=true;
 
+let gameOverScreen;
+let matrix;
+
+let resetButton;
 
 let player = {
     x:300,
@@ -45,10 +49,25 @@ function preload(){
 
 function setup(){
     createCanvas(650,1000);
-    //image( dirt,0,0);
-    //image( iron,0,0);
-   // image( oil,0,0);
     
+    matrix = [
+    //[{x:0,y:0},  {x:50,y:0},  {x:100,y:0},  {x:150,y:0},  {x:200,y:0},  {x:250,y:0},  {x:300,y:0},  {x:350,y:0},  {x:400,y:0},{x:450,y:0},  {x:500,y:0},  {x:550,y:0},  {x:600,y:0}, ],
+    [{ground: randomMaterial(),x:0,y:50}, {ground: randomMaterial(),x:50,y:50}, {ground: randomMaterial(),x:100,y:50}, {ground: randomMaterial(),x:150,y:50}, {ground: randomMaterial(),x:200,y:50}, {ground: randomMaterial(),x:250,y:50},  {ground: randomMaterial(),x:300,y:50},  {ground: randomMaterial(),x:350,y:50},  {ground: randomMaterial(),x:400,y:50},{ground: randomMaterial(),x:450,y:50},  {ground: randomMaterial(),x:500,y:50},  {ground: randomMaterial(),x:550,y:50},  {ground: randomMaterial(),x:600,y:50}],
+    [{ground: randomMaterial(),x:0,y:100},{ground: randomMaterial(),x:50,y:100},{ground: randomMaterial(),x:100,y:100},{ground: randomMaterial(),x:150,y:100},{ground: randomMaterial(),x:200,y:100}, {ground: randomMaterial(),x:250,y:100},  {ground: randomMaterial(),x:300,y:100},  {ground: randomMaterial(),x:350,y:100},  {ground: randomMaterial(),x:400,y:100},{ground: randomMaterial(),x:450,y:100},  {ground: randomMaterial(),x:500,y:100},  {ground: randomMaterial(),x:550,y:100},  {ground: randomMaterial(),x:600,y:100}],
+    [{ground: randomMaterial(),x:0,y:150},{ground: randomMaterial(),x:50,y:150},{ground: randomMaterial(),x:100,y:150},{ground: randomMaterial(),x:150,y:150},{ground: randomMaterial(),x:200,y:150}, {ground: randomMaterial(),x:250,y:150},  {ground: randomMaterial(),x:300,y:150},  {ground: randomMaterial(),x:350,y:150},  {ground: randomMaterial(),x:400,y:150},{ground: randomMaterial(),x:450,y:150},  {ground: randomMaterial(),x:500,y:150},  {ground: randomMaterial(),x:550,y:150},  {ground: randomMaterial(),x:600,y:150}],
+    [{ground: randomMaterial(),x:0,y:200},{ground: randomMaterial(),x:50,y:200},{ground: randomMaterial(),x:100,y:200},{ground: randomMaterial(),x:150,y:200},{ground: randomMaterial(),x:200,y:200}, {ground: randomMaterial(),x:250,y:200},  {ground: randomMaterial(),x:300,y:200},  {ground: randomMaterial(),x:350,y:200},  {ground: randomMaterial(),x:400,y:200},{ground: randomMaterial(),x:450,y:200},  {ground: randomMaterial(),x:500,y:200},  {ground: randomMaterial(),x:550,y:200},  {ground: randomMaterial(),x:600,y:200}],
+    [{ground: randomMaterial(),x:0,y:250},{ground: randomMaterial(),x:50,y:250},{ground: randomMaterial(),x:100,y:250},{ground: randomMaterial(),x:150,y:250},{ground: randomMaterial(),x:200,y:250}, {ground: randomMaterial(),x:250,y:250},  {ground: randomMaterial(),x:300,y:250},  {ground: randomMaterial(),x:350,y:250},  {ground: randomMaterial(),x:400,y:250}, {ground: randomMaterial(),x:450,y:250},  {ground: randomMaterial(),x:500,y:250}, {ground: randomMaterial(),x:550,y:250},  {ground: randomMaterial(),x:600,y:250}],
+    [{ground: randomMaterial(),x:0,y:300},{ground: randomMaterial(),x:50,y:300},{ground: randomMaterial(),x:100,y:300},{ground: randomMaterial(),x:150,y:300},{ground: randomMaterial(),x:200,y:300}, {ground: randomMaterial(),x:250,y:300},  {ground: randomMaterial(),x:300,y:300},  {ground: randomMaterial(),x:350,y:300},  {ground: randomMaterial(),x:400,y:300}, {ground: randomMaterial(),x:450,y:300},  {ground: randomMaterial(),x:500,y:300}, {ground: randomMaterial(),x:550,y:300},  {ground: randomMaterial(),x:600,y:300}],
+    [{ground: randomMaterial(),x:0,y:350},{ground: randomMaterial(),x:50,y:350},{ground: randomMaterial(),x:100,y:350},{ground: randomMaterial(),x:150,y:350},{ground: randomMaterial(),x:200,y:350}, {ground: randomMaterial(),x:250,y:350},  {ground: randomMaterial(),x:300,y:350},  {ground: randomMaterial(),x:350,y:350},  {ground: randomMaterial(),x:400,y:350},{ground: randomMaterial(),x:450,y:350},  {ground: randomMaterial(),x:500,y:350},  {ground: randomMaterial(),x:550,y:350},  {ground: randomMaterial(),x:600,y:350}],
+    [{ground: randomMaterial(),x:0,y:400},{ground: randomMaterial(),x:50,y:400},{ground: randomMaterial(),x:100,y:400},{ground: randomMaterial(),x:150,y:400},{ground: randomMaterial(),x:200,y:400}, {ground: randomMaterial(),x:250,y:400},  {ground: randomMaterial(),x:300,y:400},  {ground: randomMaterial(),x:350,y:400},  {ground: randomMaterial(),x:400,y:400},{ground: randomMaterial(),x:450,y:400},  {ground: randomMaterial(),x:500,y:400},  {ground: randomMaterial(),x:550,y:400},  {ground: randomMaterial(),x:600,y:400}],
+    [{ground: randomMaterial(),x:0,y:450},{ground: randomMaterial(),x:50,y:450},{ground: randomMaterial(),x:100,y:450},{ground: randomMaterial(),x:150,y:450},{ground: randomMaterial(),x:200,y:450}, {ground: randomMaterial(),x:250,y:450},  {ground: randomMaterial(),x:300,y:450},  {ground: randomMaterial(),x:350,y:450},  {ground: randomMaterial(),x:400,y:450}, {ground: randomMaterial(),x:450,y:450},  {ground: randomMaterial(),x:500,y:450}, {ground: randomMaterial(),x:550,y:450},  {ground: randomMaterial(),x:600,y:450}],
+    [{ground: randomMaterial(),x:0,y:500},{ground: randomMaterial(),x:50,y:500},{ground: randomMaterial(),x:100,y:500},{ground: randomMaterial(),x:150,y:500},{ground: randomMaterial(),x:200,y:500}, {ground: randomMaterial(),x:250,y:500},  {ground: randomMaterial(),x:300,y:500},  {ground: randomMaterial(),x:350,y:500},  {ground: randomMaterial(),x:400,y:500}, {ground: randomMaterial(),x:450,y:500},  {ground: randomMaterial(),x:500,y:500}, {ground: randomMaterial(),x:550,y:500},  {ground: randomMaterial(),x:600,y:500}],
+    [{ground: randomMaterial(),x:0,y:550},{ground: randomMaterial(),x:50,y:550},{ground: randomMaterial(),x:100,y:550},{ground: randomMaterial(),x:150,y:550},{ground: randomMaterial(),x:200,y:550}, {ground: randomMaterial(),x:250,y:550},  {ground: randomMaterial(),x:300,y:550},  {ground: randomMaterial(),x:350,y:550},  {ground: randomMaterial(),x:400,y:550}, {ground: randomMaterial(),x:450,y:550},  {ground: randomMaterial(),x:500,y:550}, {ground: randomMaterial(),x:550,y:550},  {ground: randomMaterial(),x:600,y:550}],
+    [{ground: randomMaterial(),x:0,y:600},{ground: randomMaterial(),x:50,y:600},{ground: randomMaterial(),x:100,y:600},{ground: randomMaterial(),x:150,y:600},{ground: randomMaterial(),x:200,y:600}, {ground: randomMaterial(),x:250,y:600},  {ground: randomMaterial(),x:300,y:600},  {ground: randomMaterial(),x:350,y:600},  {ground: randomMaterial(),x:400,y:600},  {ground: randomMaterial(),x:450,y:600},  {ground: randomMaterial(),x:500,y:600},{ground: randomMaterial(),x:550,y:600},  {ground: randomMaterial(),x:600,y:600}],    
+    [{ground: randomMaterial(),x:0,y:650},{ground: randomMaterial(),x:50,y:650},{ground: randomMaterial(),x:100,y:650},{ground: randomMaterial(),x:150,y:650},{ground: randomMaterial(),x:200,y:650}, {ground: randomMaterial(),x:250,y:650},  {ground: randomMaterial(),x:300,y:650},  {ground: randomMaterial(),x:350,y:650},  {ground: randomMaterial(),x:400,y:650},  {ground: randomMaterial(),x:450,y:650},  {ground: randomMaterial(),x:500,y:650},{ground: randomMaterial(),x:550,y:650},  {ground: randomMaterial(),x:600,y:650}],
+    [{ground: gold,x:0,y:700},{ground: gold,x:50,y:700},{ground: gold,x:100,y:700},{ground: gold,x:150,y:700},{ground: gold,x:200,y:700}, {ground: gold,x:250,y:700},  {ground: gold,x:300,y:700},  {ground: gold,x:350,y:700},  {ground: gold,x:400,y:700},  {ground: gold,x:450,y:700},  {ground: gold,x:500,y:700},{ground: gold,x:550,y:700},  {ground: gold,x:600,y:700}]
+
+]
 
 }
 /* attempt at turning robot
@@ -63,75 +82,70 @@ function test(){
 return playerDown
 }
 }  */
-
-function draw(){
-    background(220);
-   
-   
-    textSize(32);
-    text(`Fuel: ${fuel}`,500,50);
-    fill(51);
+ 
+function draw(){ 
     
-   image (playerNorm,player.x,player.y);
-   
 
+    background(220);
+    textSize(32);
+    text(`Fuel: ${fuel}`,500,35);
+    fill(51);
     //display gold
-    for(let i=0;i<matrix.length;i++){
-        let tempArr = matrix[11];
+    if(gameRunning){
+     
+   for(let i=0;i<matrix.length;i++){
+        let tempArr = matrix[13];
         for (let j=0; j<tempArr.length;j++){
              image(gold,tempArr[j].x,tempArr[j].y);
         }
     }
-    /*
+    
  for(let i=0;i<matrix.length-1;i++){
         let tempArr = matrix[i];
         for (let j=0; j<tempArr.length;j++){
              image(tempArr[j].ground,tempArr[j].x,tempArr[j].y);
         }
     }
-*/
+
+ image (playerNorm,player.x,player.y);
+
     
-   if (fuel <= 0){
-        noLoop();
-        gameOver();
+   if (fuel < 0){
+        resetButton =false;
+        gameRunning= false;
+       
     }
-}
-function gameOver(){
-    clear();
+}else{
     image(gameOverScreen,0,0,0,1000);
     const button = document.querySelector('#restart');
-    button.style.cssText= `visibility: visible`;
-
-    // to reset game, change position and fuel back to original value/position
+     button.style.cssText= `visibility: visible`;
+}
 }
 
 
-//creates a random ground type for each block
-const matrix = [
-    //[{x:0,y:0},  {x:50,y:0},  {x:100,y:0},  {x:150,y:0},  {x:200,y:0},  {x:250,y:0},  {x:300,y:0},  {x:350,y:0},  {x:400,y:0},{x:450,y:0},  {x:500,y:0},  {x:550,y:0},  {x:600,y:0}, ],
-    [{ground: randomMaterial(),x:0,y:50}, {ground: randomMaterial(),x:50,y:50}, {ground: randomMaterial(),x:100,y:50}, {ground: randomMaterial(),x:150,y:50}, {ground: randomMaterial(),x:200,y:50}, {ground: randomMaterial(),x:250,y:50},  {ground: randomMaterial(),x:300,y:50},  {ground: randomMaterial(),x:350,y:50},  {ground: randomMaterial(),x:400,y:50},{ground: randomMaterial(),x:450,y:50},  {ground: randomMaterial(),x:500,y:50},  {ground: randomMaterial(),x:550,y:50},  {ground: randomMaterial(),x:600,y:50}],
-    [{ground: randomMaterial(),x:0,y:100},{ground: randomMaterial(),x:50,y:100},{ground: randomMaterial(),x:100,y:100},{ground: randomMaterial(),x:150,y:100},{ground: randomMaterial(),x:200,y:100}, {ground: randomMaterial(),x:250,y:100},  {ground: randomMaterial(),x:300,y:100},  {ground: randomMaterial(),x:350,y:100},  {ground: randomMaterial(),x:400,y:100},{ground: randomMaterial(),x:450,y:100},  {ground: randomMaterial(),x:500,y:100},  {ground: randomMaterial(),x:550,y:100},  {ground: randomMaterial(),x:600,y:100}],
-    [{ground: randomMaterial(),x:0,y:150},{ground: randomMaterial(),x:50,y:150},{ground: randomMaterial(),x:100,y:150},{ground: randomMaterial(),x:150,y:150},{ground: randomMaterial(),x:200,y:150}, {ground: randomMaterial(),x:250,y:150},  {ground: randomMaterial(),x:300,y:150},  {ground: randomMaterial(),x:350,y:150},  {ground: randomMaterial(),x:400,y:150},{ground: randomMaterial(),x:450,y:150},  {ground: randomMaterial(),x:500,y:150},  {ground: randomMaterial(),x:550,y:150},  {ground: randomMaterial(),x:600,y:150}],
-    [{ground: randomMaterial(),x:0,y:200},{ground: randomMaterial(),x:50,y:200},{ground: randomMaterial(),x:100,y:200},{ground: randomMaterial(),x:150,y:200},{ground: randomMaterial(),x:200,y:200}, {ground: randomMaterial(),x:250,y:200},  {ground: randomMaterial(),x:300,y:200},  {ground: randomMaterial(),x:350,y:200},  {ground: randomMaterial(),x:400,y:200},{ground: randomMaterial(),x:450,y:200},  {ground: randomMaterial(),x:500,y:200},  {ground: randomMaterial(),x:550,y:200},  {ground: randomMaterial(),x:600,y:200}],
-    [{ground: randomMaterial(),x:0,y:250},{ground: randomMaterial(),x:50,y:250},{ground: randomMaterial(),x:100,y:250},{ground: randomMaterial(),x:150,y:250},{ground: randomMaterial(),x:200,y:250}, {ground: randomMaterial(),x:250,y:250},  {ground: randomMaterial(),x:300,y:250},  {ground: randomMaterial(),x:350,y:250},  {ground: randomMaterial(),x:400,y:250}, {ground: randomMaterial(),x:450,y:250},  {ground: randomMaterial(),x:500,y:250}, {ground: randomMaterial(),x:550,y:250},  {ground: randomMaterial(),x:600,y:250}],
-    [{ground: randomMaterial(),x:0,y:300},{ground: randomMaterial(),x:50,y:300},{ground: randomMaterial(),x:100,y:300},{ground: randomMaterial(),x:150,y:300},{ground: randomMaterial(),x:200,y:300}, {ground: randomMaterial(),x:250,y:300},  {ground: randomMaterial(),x:300,y:300},  {ground: randomMaterial(),x:350,y:300},  {ground: randomMaterial(),x:400,y:300}, {ground: randomMaterial(),x:450,y:300},  {ground: randomMaterial(),x:500,y:300}, {ground: randomMaterial(),x:550,y:300},  {ground: randomMaterial(),x:600,y:300}],
-    [{ground: randomMaterial(),x:0,y:350},{ground: randomMaterial(),x:50,y:350},{ground: randomMaterial(),x:100,y:350},{ground: randomMaterial(),x:150,y:350},{ground: randomMaterial(),x:200,y:350}, {ground: randomMaterial(),x:250,y:350},  {ground: randomMaterial(),x:300,y:350},  {ground: randomMaterial(),x:350,y:350},  {ground: randomMaterial(),x:400,y:350},{ground: randomMaterial(),x:450,y:350},  {ground: randomMaterial(),x:500,y:350},  {ground: randomMaterial(),x:550,y:350},  {ground: randomMaterial(),x:600,y:350}],
-    [{ground: randomMaterial(),x:0,y:400},{ground: randomMaterial(),x:50,y:400},{ground: randomMaterial(),x:100,y:400},{ground: randomMaterial(),x:150,y:400},{ground: randomMaterial(),x:200,y:400}, {ground: randomMaterial(),x:250,y:400},  {ground: randomMaterial(),x:300,y:400},  {ground: randomMaterial(),x:350,y:400},  {ground: randomMaterial(),x:400,y:400},{ground: randomMaterial(),x:450,y:400},  {ground: randomMaterial(),x:500,y:400},  {ground: randomMaterial(),x:550,y:400},  {ground: randomMaterial(),x:600,y:400}],
-    [{ground: randomMaterial(),x:0,y:450},{ground: randomMaterial(),x:50,y:450},{ground: randomMaterial(),x:100,y:450},{ground: randomMaterial(),x:150,y:450},{ground: randomMaterial(),x:200,y:450}, {ground: randomMaterial(),x:250,y:450},  {ground: randomMaterial(),x:300,y:450},  {ground: randomMaterial(),x:350,y:450},  {ground: randomMaterial(),x:400,y:450}, {ground: randomMaterial(),x:450,y:450},  {ground: randomMaterial(),x:500,y:450}, {ground: randomMaterial(),x:550,y:450},  {ground: randomMaterial(),x:600,y:450}],
-    [{ground: randomMaterial(),x:0,y:500},{ground: randomMaterial(),x:50,y:500},{ground: randomMaterial(),x:100,y:500},{ground: randomMaterial(),x:150,y:500},{ground: randomMaterial(),x:200,y:500}, {ground: randomMaterial(),x:250,y:500},  {ground: randomMaterial(),x:300,y:500},  {ground: randomMaterial(),x:350,y:500},  {ground: randomMaterial(),x:400,y:500}, {ground: randomMaterial(),x:450,y:500},  {ground: randomMaterial(),x:500,y:500}, {ground: randomMaterial(),x:550,y:500},  {ground: randomMaterial(),x:600,y:500}],
-    [{ground: randomMaterial(),x:0,y:550},{ground: randomMaterial(),x:50,y:550},{ground: randomMaterial(),x:100,y:550},{ground: randomMaterial(),x:150,y:550},{ground: randomMaterial(),x:200,y:550}, {ground: randomMaterial(),x:250,y:550},  {ground: randomMaterial(),x:300,y:550},  {ground: randomMaterial(),x:350,y:550},  {ground: randomMaterial(),x:400,y:550}, {ground: randomMaterial(),x:450,y:550},  {ground: randomMaterial(),x:500,y:550}, {ground: randomMaterial(),x:550,y:550},  {ground: randomMaterial(),x:600,y:550}],
-    [{ground: randomMaterial(),x:0,y:600},{ground: randomMaterial(),x:50,y:600},{ground: randomMaterial(),x:100,y:600},{ground: randomMaterial(),x:150,y:600},{ground: randomMaterial(),x:200,y:600}, {ground: randomMaterial(),x:250,y:600},  {ground: randomMaterial(),x:300,y:600},  {ground: randomMaterial(),x:350,y:600},  {ground: randomMaterial(),x:400,y:600},  {ground: randomMaterial(),x:450,y:600},  {ground: randomMaterial(),x:500,y:600},{ground: randomMaterial(),x:550,y:600},  {ground: randomMaterial(),x:600,y:600}]
+
+function resetGame(){
+   
+    resetButton=true;
+    gameRunning=true;
+    player.x = 300;
+    player.y = 0;
+    fuel = 10;
+    document.querySelector('#restart').style.cssText= `visibility: hidden`;
+   
     
-]
+   console.log("hello");
+   console.log(player);
+
+}
 
 
- 
 
 function randomMaterial(){
     let rand= randomNum(100);
     if (rand>=10){
         return dirt;
-    }else if(rand<=5){
+    }else if(rand<=4){
         return iron;
     }else{
         return oil;
@@ -147,26 +161,35 @@ function randomNum(max){
 function keyPressed(){
     if(keyCode === LEFT_ARROW){
         player.x===0 ? null :player.x-= 50;
-        
+        fuel= fuel - 1;
 
     }else if ( keyCode === RIGHT_ARROW){
         player.x===600 ? null : player.x+= 50;
-       
+        fuel= fuel - 1;
     }  else if (keyCode === DOWN_ARROW){
-      
+        fuel= fuel - 1;
      player.y+= 50;
     }
-    fuel= fuel - 1;
-    if (player.y===400 && player.x===400){
-        fuel = 10;
+    
+
+    for(let i=0;i<matrix.length-1;i++){
+        let tempArr = matrix[i];
+        for (let j=0; j<tempArr.length;j++){
+           if ( player.x===tempArr[j].x &&
+                player.y===tempArr[j].y &&
+                tempArr[j].ground=== oil){
+                    //if you take oil replace with dirt
+                    tempArr[j].ground = dirt
+            fuel += 10; 
+        }else if(player.y===700 ){
+                console.log("win");
+                //winScreen()
+
+            }
+        }
     }
-    
-
-
     console.log(player); 
-    
-    
-    
-  
+    console.log(fuel);
+ 
   }
 
